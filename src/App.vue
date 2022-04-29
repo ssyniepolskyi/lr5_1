8088abe18b9611579e4bd17ee7d1b12d77d6024d
Serafym Syniepolskyi <serafym_syniepolskyi@epam.com>
2022-04-29 14:44:51 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Stocks from './components/Stocks.vue'
+</script>
+
+<template>
+  <div class = "container">
+    <h2 class = "title"> Діючі банки України</h2>
+    <Stocks />
+  </div>
+</template>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="stock">
+    <div class="stock-item" v-for="value in stock" :key="value.stock">
+      <div class="stock-item__info">
+        <div class="stock-item__cover">
+          <img :src="value.IMAGE" alt="Logo Company">
+        </div>
+        <h3 class="stock-item__title">
+          {{value.SHORTNAME}}
+          <span>{{value.NP}}</span>
+          <span>{{value.ADRESS}}</span>
+        </h3>
+      </div>
+      <span class="stock-item__erdpou">ЄРДПУ: {{value.KOD_EDRPOU }}</span>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  name: 'Stocks',
+  data(){
+    return{
+      stock: [],
+      errors: [],
+    }
+  },
+  created(){
+    axios.get('src/components/data.json')
+    .then(response =>{
+      this.stock = response.data
+      console.log(response)
+    })
+  }
+} as any
+
+</script>
